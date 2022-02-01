69e26763b22cd618533151cab08cd5b451252d5e
Tim Miller <tim.miller@gmail.com>
2022-02-01 07:37:40 -0800

    Remove duplicate encryption from s3 upload

--- upload_ctakes_output.sh
+++ upload_ctakes_output.sh
@@ -8,12 +8,14 @@ s3_output_path=`$aws ec2 describe-tags --filters Name=key,Values=s3_out | jq --r
 keyId=$($aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:718952877825:secret:master-fWTVY2 --output text --query SecretString | jq --raw-output '."'$2'-kms-s3"')
 
 for fn in $1/*.json; do
-    aws-encryption-cli --encrypt --input $fn --wrapping-keys key=$keyId --encryption-context purpose=test --metadata-output `basename $fn`.metadata --output $fn.encrypted
-    $aws s3 cp $fn.encrypted s3://$s3_bucket/$s3_output_path/ --sse aws:kms --sse-kms-key-id $keyId
+    #aws-encryption-cli --encrypt --input $fn --wrapping-keys key=$keyId --encryption-context purpose=test --metadata-output `basename $fn`.metadata --output $fn.encrypted
+    #$aws s3 cp $fn.encrypted s3://$s3_bucket/$s3_output_path/ --sse aws:kms --sse-kms-key-id $keyId
+    $aws s3 cp $fn s3://$s3_bucket/$s3_output_path/ --sse aws:kms --sse-kms-key-id $keyId
 done
 
 for fn in $1/*.csv; do
-    aws-encryption-cli --encrypt --input $fn --wrapping-keys key=$keyId --encryption-context purpose=test --metadata-output `basename $fn`.metadata --output $fn.encrypted
-    $aws s3 cp $fn.encrypted s3://$s3_bucket/$s3_output_path/ --sse aws:kms --sse-kms-key-id $keyId
+    #aws-encryption-cli --encrypt --input $fn --wrapping-keys key=$keyId --encryption-context purpose=test --metadata-output `basename $fn`.metadata --output $fn.encrypted
+    #$aws s3 cp $fn.encrypted s3://$s3_bucket/$s3_output_path/ --sse aws:kms --sse-kms-key-id $keyId
+    $aws s3 cp $fn s3://$s3_bucket/$s3_output_path/ --sse aws:kms --sse-kms-key-id $keyId
 done
 
