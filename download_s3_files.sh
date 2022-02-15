610b16f2f537ebb951fc25da9516395f3c2024ae
Tim Miller <timothy.miller@childrens.harvard.edu>
2022-02-15 23:27:43 +0000

    Grab resource id before checking tags so we can get more specific information.

--- download_s3_files.sh
+++ download_s3_files.sh
@@ -3,8 +3,9 @@
 # May have to also set AWS key parameters here, potentially passed in as tags as well.
 aws=/usr/local/bin/aws
 
+resourceid=`curl http://169.254.169.254/latest/meta-data/instance-id`
 s3_bucket=`$aws ec2 describe-tags --filters Name=key,Values=s3_bucket | jq --raw-output '.Tags[0].Value'`
-s3_input_path=`$aws ec2 describe-tags --filters Name=key,Values=s3_in | jq --raw-output '.Tags[0].Value'`
+s3_input_path=`$aws ec2 describe-tags --filters Name=key,Values=s3_in Name=resource-id,Values=$resourceid | jq --raw-output '.Tags[0].Value'`
 
 $aws s3 cp --recursive s3://${s3_bucket}/${s3_input_path} /data/
 
