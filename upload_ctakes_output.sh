36da8b9a6a2c0f7187d5205fe88a5d6a1adfffdc
Tim Miller <tim.miller@gmail.com>
2022-02-18 16:53:42 -0800

    Fix upload script to use its resource ID to get the correct upload path.

--- upload_ctakes_output.sh
+++ upload_ctakes_output.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+resourceid=`curl http://169.254.169.254/latest/meta-data/instance-id`
+
 # May have to also set AWS key parameters here, potentially passed in as tags as well.
 aws=/usr/local/bin/aws
 
 s3_bucket=`$aws ec2 describe-tags --filters Name=key,Values=s3_bucket | jq --raw-output '.Tags[0].Value'`
-s3_output_path=`$aws ec2 describe-tags --filters Name=key,Values=s3_out | jq --raw-output '.Tags[0].Value'`
+s3_output_path=`$aws ec2 describe-tags --filters Name=key,Values=s3_out Name=resource-id,Values=$resourceid | jq --raw-output '.Tags[0].Value'`
 keyId=$($aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:718952877825:secret:master-fWTVY2 --output text --query SecretString | jq --raw-output '."'$2'-kms-s3"')
 
 find $1 -name "*.json" -exec cat {} \; >> /data/all.json
